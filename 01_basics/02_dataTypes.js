088c501fa84a4ef3ac0393483c574a3624215471
Astha Kumari <131004274+astha1503@users.noreply.github.com>
2025-07-14 14:57:35 +0000

    datatypes done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as newer version
+// alert(3+3)  // we are using nodejs not browser
+
+// code readability should be high
+
+let name = "Astha"
+let age = 20
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+//null => standalone value (it also trate as or type  object)
+// undefined => 
+// symbol => for unique
+
+// object
+
+console.log(typeof age)
+console.log(typeof undefined) // undefined
+console.log(typeof null); // object
