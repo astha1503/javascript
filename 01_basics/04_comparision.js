8701f891d81220814c49c16d07f2ab33dba3bcc8
Astha Kumari <131004274+astha1503@users.noreply.github.com>
2025-07-15 11:35:47 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,18 @@
+// console.log(2>1); // true
+
+// trye to to comaparision in same data type
+
+// comparision with null and undefined can also give unpedicated value
+
+// == in this value can be typecasted and vlue also give true but
+// === it strictly check the value as well as datatype of value .
+// Don't prefere to this comparsion
+
+console.log(null > 0);
+console.log(null == 0)
+console.log(null >= 0);
+
+console.log(undefined == 0)
+console.log(undefined > 0);
+console.log(undefined >0);
+
