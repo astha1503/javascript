a69c9e5abedcc187c52da3b001a3679d4e1d9c4f
Astha Kumari <131004274+astha1503@users.noreply.github.com>
2025-07-15 12:21:42 +0000

    memory uses

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -53,3 +53,29 @@ console.log(typeof bigNumber);
 // 8. Object(host and does not implement [[Call]])  : Implementation-defined except may not be "undefined" , "boolean" , "number" , or "sting" .
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+//////////////////////////////////////////////////
+
+// stack (Primitive) # in this we get copyed value, Heap 3 but here it give value by refrance (Non-Primitive)
+
+let myPetName = "Pyari";
+
+let anotherPet = myPetName;
+anotherPet = "gola";
+
+console.log(myPetName);
+console.log(anotherPet); // it pass copy value change in copy
+
+let userOne = {
+    email : "user@gmail.com" ,
+    upi : "user2ybl"
+}
+
+let userTwo = userOne; // here value is pass by refrace if we change in userTwo it will also reflected into userOne
+
+userTwo.email = "astha@google.com";
+console.log(userOne.email);
+console.log (userTwo.email); // but here value is same and also it pass by refration so if we change one value it show to other
+
+
